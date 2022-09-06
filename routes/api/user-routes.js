8b72953d2f3c95f95d9f1b38a14b4639c3efbe76
Christian Pena <Chip@Christians-MBP.attlocal.net>
2022-09-06 14:51:13 -0500

    Comments and Routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -15,9 +15,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     User.findOne({
         where: {
-            attribute: { exclude: ['password'] },
+            attributes: { exclude: ['password'] },
             id:req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if(!dbUserData) {
